7b56bb1fc972a4193c3397a62cc00aa0bd60ab35
day-bug <1127130679@qq.com>
2019-11-28 11:39:01 +0800

    实体类增加

---
+++ src/main/java/com/gaoshu/pojo/team.java
@@ -0,0 +1,30 @@
+package com.gaoshu.pojo;
+
+import lombok.Data;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Data
+@Table(name = "team")
+public class team implements Serializable {
+    @Id
+    @GeneratedValue(generator = "JDBC")
+    private Integer tId;//球队id
+    private String tName;//名称
+    private String tAddr;//所在地
+    private String tPhone;//联系方式
+    private String tSynopsis;//球队简介
+    private String tIcon;//球队图标
+    private String tSlogan;//比赛口号
+    private Integer tCount;//比赛场次
+    private Integer tWins;//剩
+    private Integer tDraws;//平
+    private Integer tLosses;//负
+    private Integer tAp;//积分
+    private String tStatus;//状态
+
+
+}
---
+++ src/main/java/com/gaoshu/pojo/users.java
@@ -0,0 +1,25 @@
+package com.gaoshu.pojo;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+//用户表
+@Data
+@Table(name = "users")
+public class users implements Serializable {
+   @Id
+   @GeneratedValue(generator = "JDBC")
+    private Integer uId; //用户ID
+    private String uName;
+    private String uRealname;
+    private String uPass;
+    private String uEmail;
+    private String uPhone;
+    private String uQq;
+    private Integer tId;//球队id
+    private Integer uLeader;//是否队长
+}
